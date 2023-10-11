779103b944c999672ffbbdde0b5ea5f429f2997d
anjana732 <anjanasingh1257@gmail.com>
2023-10-11 18:05:49 +0530

    Connected Successfully to database

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $desc = $_POST['desc'];
-    $sql = "INSERT INTO 'Trip'.'trip' (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    $sql = "INSERT INTO `Trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
     echo $sql;
 
     if($con->query($sql)== true){
